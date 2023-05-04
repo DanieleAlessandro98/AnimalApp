e98aaf02086ca241c93595e5d605759657373d9c
giuseppe liace <giuseppeliace10@gmail.com>
2023-05-04 19:41:59 +0200

    third

--- app/src/main/java/it/uniba/dib/sms222334/Activity/LoginActivity.java
+++ app/src/main/java/it/uniba/dib/sms222334/Activity/LoginActivity.java
@@ -1,16 +1,44 @@
 package it.uniba.dib.sms222334.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import it.uniba.dib.sms222334.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    final static String TAG="LoginActivitybochicchio";
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_layout);
+        setContentView(R.layout.private_register);
+
+        db.collection("Private")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
     }
 }
